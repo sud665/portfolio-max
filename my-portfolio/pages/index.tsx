0068dc21213d2400d797b1143aa13dfa7cf5a67a
Max <sud665@ymjlab.com>
2023-08-08 23:27:08 +0900

    기획준비

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -5,9 +5,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
-        <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-            <h1>포트폴리오 자동배포 설정 </h1>
-            <h1>개발 환경 에서 배포</h1>
+        <main className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}>
+            <h1>포트폴리오 자동배포 설정 @@ </h1>
+            <p>이젠 기획단계 입니다.</p>
         </main>
     );
 }
