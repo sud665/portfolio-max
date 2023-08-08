129cc3fda5add1abd46f68bfbf3d8993f53faa2c
Max <sud665@ymjlab.com>
2023-08-08 23:23:21 +0900

    개발 환경 배포

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     return (
         <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
             <h1>포트폴리오 자동배포 설정 </h1>
+            <h1>개발 환경 에서 배포</h1>
         </main>
     );
 }
