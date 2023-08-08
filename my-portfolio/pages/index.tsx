cb8c5fd838f93cc396d698d03b3a9158017e5a98
Max <sud665@ymjlab.com>
2023-08-08 23:15:06 +0900

    자동배포 설정

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
     return (
         <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-            <h1>포트폴리오 제작중 </h1>
+            <h1>포트폴리오 자동배포 설정 </h1>
         </main>
     );
 }
