759e859d030b7e52b4c0852f86e24c4cb80511b1
Max <sud665@ymjlab.com>
2023-08-15 15:37:39 +0900

    메인타이틀 변경

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
+    const title: string = "{Hello World :)}";
+
     return (
-        <main className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}>
-            <h1>포트폴리오 자동배포 설정 @@ </h1>
-            <p>이젠 기획단계 입니다.</p>
+        <main className="flex h-screen w-screen flex-col items-center justify-center">
+            <h1>{title}</h1>
         </main>
     );
 }
